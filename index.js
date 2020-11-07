daa9b44a01f4b344230c32e693cece530d4982e6
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 01:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ function theBeatlesPlay(musicians, instruments) {
   return arr;
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function johnLennonFacts(facts) {
-  
+  let counter = 0;
+  let modifiedFacts = [];
+  while(counter < facts.length) {
+    modifiedFacts.push(`${facts[counter]}!!!`);
+    counter++;
+  }
 }
